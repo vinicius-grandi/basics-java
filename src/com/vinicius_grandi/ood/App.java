80b571a29ac7edd72e3b52e4a74fab4603383f83
Vinícius Gustavo Grandi <93281527+vinicius-grandi@users.noreply.github.com>
2022-11-16 18:35:39 -0300

    creates a java.util list

--- src/com/vinicius_grandi/ood/App.java
+++ src/com/vinicius_grandi/ood/App.java
@@ -1,5 +1,8 @@
 package com.vinicius_grandi.ood;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     private static void login(User user) {
         System.out.println(user.getFullName() + " is logged in");
@@ -12,10 +15,31 @@ public class App {
     }
 
     private static Guitar createNewGuitar(String brand, int strings) {
-        var guitar = new Guitar();
-        guitar.brand = brand;
-        guitar.strings = strings;
+        var guitar = new Guitar("fender", 6);
         return guitar;
+    }
+
+    private static void getAllGuitars() {
+        // type List<> is used to create a list with undetermined length
+        List<Guitar> guitars = new ArrayList<>();
+        for (int i = 0; i < 10; i += 1) {
+            guitars.add(new Guitar("Fender", i));
+        }
+
+        for (Guitar guitar : guitars) {
+            System.out.println(guitar.getBrand());
+        }
+    }
+
+    private static void getAllUsers() {
+        User[] users = new User[10];
+        for (int i = 0; i < users.length; i++) {
+            User actual = new User("nome" + i, "sobrenome" + i);
+            users[i] = actual;
+        }
+        for (User user : users) {
+            System.out.println(user.getFullName());
+        }
     }
 
     public static void main(String[] args) {
@@ -27,5 +51,6 @@ public class App {
 
         logout(userA);
         logout(userB);
+        getAllGuitars();
     }
 }
--- src/com/vinicius_grandi/ood/Guitar.java
+++ src/com/vinicius_grandi/ood/Guitar.java
@@ -1,10 +1,18 @@
 package com.vinicius_grandi.ood;
 
 public class Guitar {
-    public String brand;
-    public int strings;
+    private String brand;
+    private int strings;
 
+    Guitar(String brand, int strings) {
+        this.brand = brand;
+        this.strings = strings;
+    }
     public void play() {
         System.out.println("The guitar is playing lmao");
+    }
+
+    public String getBrand() {
+        return this.brand;
     }
 }
