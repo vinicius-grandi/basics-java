8ada7d0caf820ba25d9b5cc7597b5eab2ebd3a0a
Vinícius Gustavo Grandi <93281527+vinicius-grandi@users.noreply.github.com>
2022-11-16 22:56:43 -0300

    create a new array filled through Arrays.fill method

--- src/com/vinicius_grandi/ood/CoolNumber.java
+++ src/com/vinicius_grandi/ood/CoolNumber.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class CoolNumber {
     public static void main(String[] args) {
         int[] numbers = {2, 4, -1, 3, 0, 111};
-        int[] sortedNumbers = {-1, 0, 2, 4, 2, 111};
+        int[] sortedNumbers = {-1, 0, 2, 3, 4, 111};
         // it sorts the array, modifying the original reference
         Arrays.sort(numbers);
 
@@ -14,5 +14,12 @@ public class CoolNumber {
 
         // it performs a deep comparison between two arrays
         System.out.println(Arrays.equals(numbers, sortedNumbers));
+
+        int[] aLotOfZeros = new int[10];
+        // it fills an array with the specified value
+        Arrays.fill(aLotOfZeros, 0);
+        for (int zero : aLotOfZeros) {
+            System.out.println(zero);
+        }
     }
 }
